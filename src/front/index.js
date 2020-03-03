5ee6d0dc5f78680dae483e2e32d419155066497e
Midias <midias.dev@gmail.com>
2020-03-03 11:18:07 +0100

    remove index.js

--- src/front/index.js
+++
@@ -1,4 +0,0 @@
-fetch("http://127.0.0.1:3000")
-    .then(result => result.json())
-    .then(result => console.log(result))
-    .then(result => jquery('#qrcode').qrcode(`${result.id} : ${result.code} / ${result.discountRate}`))
